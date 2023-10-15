267f4b680d6b756c2212c8564606395ddf5ab3b3
Konstantin Khakhilev <constantin.khakhilev@gmail.com>
2023-10-15 21:17:52 +0700

    Спринт 7/19: 7 спринт → Тема 2/3: Работа с формами → Урок 6/10

--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -5,6 +5,6 @@ from .forms import ContestForm
 
 def proposal(request):
     template = 'contest/form.html'
-    form = ContestForm(request.GET or None)
+    form = ContestForm(request.POST or None)
     context = {'form': form}
     return render(request, template, context)
